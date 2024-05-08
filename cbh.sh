57e43404c61609dfdb4fa84dd1d61ab55b4f5b86
Rahul Wijesurendra <167352879+wijathemidja@users.noreply.github.com>
2024-05-08 06:46:45 +0100

    Add files via upload

---
+++ cbh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo “CHROMIE HACK INITIATING”
+echo “Hi this is Wija”
+echo “Basically this is just installing other people’s program for you because your brain (no offence) is kinda small.”
+echo “it should be installing now…”
+sudo apt-get update -y
+sudo apt-get install nsnake -y
+sudo apt-get install gnome-2048 -y
+sudo apt-get install dolphin -y
+sudo apt-get install chromium -y
+echo “The programs should now be installed!”
+echo “n-snake is a snake game in your terminal, 2048 is 2048 (I got 4096), dolphin is a file manager and chromium lets you run extensions and sometimes get past the firewall”
+echo “CBH by Wij was successfully run (maybe)”
