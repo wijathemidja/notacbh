49641eeda64db08fd7cc92fe053da893c8c75369
Rahul Wijesurendra <167352879+wijathemidja@users.noreply.github.com>
2024-05-08 09:54:01 +0100

    Update notacbhb.sh

--- notacbhb.sh
+++ notacbhb.sh
@@ -9,6 +9,8 @@ sudo apt-get install nsnake -y
 sudo apt-get install gnome-2048 -y
 sudo apt-get install dolphin -y
 sudo apt-get install chromium -y
+sudo apt-get update --fix-missing
+sudo apt-get install chromium
 echo “The programs should now be installed”
 echo “n-snake is a snake game with your terminal, 2048 is 2048 I got 4096, dolphin is a file manager and chromium lets you run extensions and sometimes get past the firewall”
 echo “CBH by Wij was successfully run maybe”
