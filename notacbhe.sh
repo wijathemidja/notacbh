bd69bb5d66ee91b7f042bed99a094df89172e3f9
Rahul Wijesurendra <167352879+wijathemidja@users.noreply.github.com>
2024-06-18 13:38:38 +0100

    Create notacbhe.sh

---
+++ notacbhe.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+echo “CHROMIE HACK INITIATING”
+echo “Hi this is Wija”
+echo “Basically this is just installing other people’s program 4 you because your brain no offence is kinda small.”
+echo “it should be installing now…”
+sudo apt-get update -y
+sudo apt-get install dolphin -y
+sudo apt-get install chromium -y
+sudo apt-get update --fix-missing
+sudo apt-get install chromium -y
+echo “The programs should now be installed”
+echo "This is the Essential package"
+echo “CBH by Wij was successfully run maybe”
