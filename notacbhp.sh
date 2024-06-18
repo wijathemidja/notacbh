cdfcac37f53c5267b99071f548de4b10159906c5
Rahul Wijesurendra <167352879+wijathemidja@users.noreply.github.com>
2024-06-18 13:37:44 +0100

    Update notacbhp.sh

--- notacbhp.sh
+++ notacbhp.sh
@@ -1,1 +1,18 @@
 
+#!/bin/bash
+
+echo “CHROMIE HACK INITIATING”
+echo “Hi this is Wija”
+echo “Basically this is just installing other people’s program 4 you because your brain no offence is kinda small.”
+echo “it should be installing now…”
+sudo apt-get update -y
+sudo apt-get install nsnake -y
+sudo apt-get install gnome -y
+sudo apt-get install dolphin -y
+sudo apt-get install chromium -y
+sudo apt-get update --fix-missing
+sudo apt-get install chromium -y
+echo “The programs should now be installed”
+echo “this is the plus package”
+echo “CBH by Wij was successfully run maybe”
+
